c1198ccb09d7b4bb744947410eba32acdba1d090
subzero <shubham8111994@gmail.com>
2023-05-11 17:37:05 +0530

    Initial commit

---
+++ Sum.swift
@@ -0,0 +1,10 @@
+//Write a Swift program to compute the sum of the two integers. 
+//If the values are equal return the triple their sum.
+
+var val1: Int = 20
+var val2: Int = 20
+if (val1 == val2){
+    print("Values are equal, sum is \(3*(val1+val2))")
+} else {
+    print("sum is \((val1+val2))")
+}
