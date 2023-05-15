2f0d4bc1b7c2ef35817332f6bbd667c56ca2e1ef
subzero <shubham8111994@gmail.com>
2023-05-15 16:07:37 +0530

    Added program to create a structure demo

---
+++ StructDemo.swift
@@ -0,0 +1,11 @@
+// Program structure demo    
+struct  Amount  
+{  
+   var rupees: Int  
+   var paisa: Int  
+}  
+var x = Amount (rupees: 10000, paisa: 50)  
+print(x)  
+print("Amount in rupees is \(x.rupees)")  
+print("Amount in paisa is \(x.paisa)")  
+
