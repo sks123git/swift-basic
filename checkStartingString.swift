9bcf90a7b4c42ba578be693c7ab5434d5ac325c4
subzero <shubham8111994@gmail.com>
2023-05-15 11:52:24 +0530

    Added program to test a given string whether it starts with Is. Return true or false.

---
+++ checkStartingString.swift
@@ -0,0 +1,9 @@
+// Program to test a given string whether it starts with "Is". Return true or false.
+
+func checkString(str: String) -> Bool{
+    if(str.hasPrefix("Is"))
+    {return true}
+    else
+    {return false}
+}
+print(checkString(str: "IsShubham"))
